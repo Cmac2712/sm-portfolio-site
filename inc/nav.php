58b7d6a799ca5e930f2e2a0af87879a352235c81
Craig <craig.macintyre@indez.com>
2018-11-11 18:08:03 +0000

    Changed to html to php

---
+++ inc/nav.php
@@ -0,0 +1,72 @@
+        <!--
+            START: Navbar
+
+            Additional Classes:
+                .dk-navbar-sticky
+                .dk-navbar-transparent
+                .dk-navbar-white
+                .dk-navbar-fullscreen || .dk-navbar-(xl, lg, md, sm)-fullscreen
+        -->
+        <nav class="dk-navbar dk-navbar-sticky dk-navbar-lg-fullscreen">
+            <div class="container">
+                <div class="dk-nav-content">
+                    
+                    <a href="/" class="dk-nav-logo">SAM&nbsp;MACINTYRE</a>
+                    
+                    
+                    
+            <ul class="dk-nav dk-nav-align-right">
+                <!-- LINK ONE -->
+                <li class="dk-drop-item active">
+                    <a href="/">
+                        Work
+                    </a>
+                </li>
+                <!-- LINK TWO -->
+                <li class="dk-drop-item">
+                    <a href="about-me.html">
+                        About Me
+                    </a>
+                </li>
+                <!-- LINK THREE -->
+                <li class="dk-drop-item">
+                    <a href="contact.html">
+                        Contact
+                    </a>
+                </li>  
+                <!-- LINK FOUR -->
+                <li class="dk-drop-item">
+                    <a href="https://www.linkedin.com/in/samcoton/">
+                        <span class="fab fa-linkedin"></span>
+                    </a>
+                </li>
+                 <!-- LINK FIVE -->
+                <li class="dk-drop-item">
+                    <a href="https://www.instagram.com/smac_design/">
+                        <span class="fab fa-instagram"></span>
+                    </a>
+                </li>
+            </ul>
+                    
+                </div>
+            </div>
+        </nav>
+
+<div class="dk-navbar dk-navbar-fullscreen">
+    <div class="container">
+        <div class="d-block"></div>
+        <div class="dk-nav-content">
+            <!-- This will copy the contents of the dk-nav-content -->
+        </div>
+        <ul class="dk-social-links text-center">
+            <li><a href="#" class="dk-social-facebook"><span class="fab fa-facebook-f"></span></a></li>
+            <li><a href="#" class="dk-social-twitter"><span class="fab fa-twitter"></span></a></li>
+            <li><a href="#" class="dk-social-google-plus"><span class="fab fa-google-plus-g"></span></a></li>
+            <li><a href="#" class="dk-social-behance"><span class="fab fa-behance"></span></a></li>
+            <li><a href="#" class="dk-social-pinterest"><span class="fab fa-pinterest"></span></a></li>
+            <li><a href="#" class="dk-social-instagram"><span class="fab fa-instagram"></span></a></li>
+        </ul>
+    </div>
+</div>	
+       
+        <!-- END: Navbar -->
